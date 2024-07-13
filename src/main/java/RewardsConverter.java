b303e8d739d91ccfbbf0553902baf229f4ab9df1
Soumadityaghosh <soumadityaghosh700@gmail.com>
2024-07-14 03:28:54 +0530

    Update RewardsConverter.java

--- src/main/java/RewardsConverter.java
+++ src/main/java/RewardsConverter.java
@@ -1,11 +1,10 @@
 import java.util.Scanner;
-
 public class RewardsConverter {
-    public static void main(String[] args) {
-        var scanner = new Scanner(System.in);
+    public static void main(String args[]) {
+        Scanner var = new Scanner(System.in);
         System.out.println("Welcome to the Credit Card Rewards Converter!");
         System.out.println("Please enter a cash value to convert to airline miles: ");
-        var input_value = scanner.nextLine();
+        int input_value = var.nextLine();
         double cashValue;
         try {
             cashValue = Double.parseDouble(input_value);
